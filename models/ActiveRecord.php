a6843832c5cdb3f4866c2bd484310013b22b7f47
Ruben Dario Hernandez Gonzalez <gonzalezruben468@gmai.com>
2026-09-12 18:33:13 -0500

    Test para crear Tareas realizado

--- models/ActiveRecord.php
+++ models/ActiveRecord.php
@@ -66,6 +66,7 @@ class ActiveRecord {
     // Busqueda Where con Columna 
     public static function where($columna, $valor) {
         $query = "SELECT * FROM " . static::$tabla . " WHERE $columna = '$valor'";
+        
         $resultado = self::consultarSQL($query);
         return array_shift( $resultado ) ;
     }
--- tests/TareaTest.php
+++ tests/TareaTest.php
@@ -1,9 +1,35 @@
 <?php
 
-namespace tests;
-
+use Controllers\CitaController;
+use Model\Tareas;
 use PHPUnit\Framework\TestCase;
+use MVC\Router;
 
-class TareaTest extends TestCase {
-    
+// 1. Mover el require_once FUERA del método para evitar re-definiciones críticas
+require_once __DIR__ . '/../includes/app.php';
+
+class TareaTest extends TestCase
+{
+
+    public function test_agregar_tarea()
+    {
+        // 2. Simular variables de entorno globales
+        $_SERVER['REQUEST_METHOD'] = 'POST';
+        $_SERVER['PATH_INFO'] = '/crearTarea';
+        $_SERVER['REQUEST_URI'] = '/crearTarea';
+
+        $datos = [
+            'nombretarea' => 'Prueba de datos',
+            'usuarioId' => '1'
+        ];
+
+        $router = new Router();
+        $router->post('/crearTarea', [CitaController::class, 'crearTarea']);
+
+        $_POST = $datos;
+
+        ob_start();
+        $router->comprobarRutas();
+        $respuestaDelControlador = ob_get_clean();
+    }
 }
